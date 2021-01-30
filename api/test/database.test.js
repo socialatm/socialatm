f87b79eaf8faaf9cb3c5fdd97cbab85d23e9cc3c
Ray <raypeaslee@gmail.com>
2021-01-30 16:56:20 -0500

    Update database.test.js

--- api/test/database.test.js
+++ api/test/database.test.js
@@ -6,3 +6,10 @@ describe('Array', function() {
     });
   });
 });
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    // pending test below
+    it('should return -1 when the value is not present');
+  });
+});
